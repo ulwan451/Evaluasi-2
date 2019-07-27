f0a0d9e406e91de52627a6d0c09cf8d5af8fada7
ulwan451 <52684583+ulwan451@users.noreply.github.com>
2019-07-27 16:07:35 +0700

    Update evaluasi4dua.js

--- evaluasi4dua.js
+++ evaluasi4dua.js
@@ -1,3 +1,12 @@
+var capitalize = function (string)
+
+var capital = capitalize.map(function(string) {
+    return string.capital(toUpperCase())
+});
+
+document.write(capital)
+
+
 //var capitalize = function (string) {
  //   if (arguments.length === 1) {
 //        return string.toUpperCase();
